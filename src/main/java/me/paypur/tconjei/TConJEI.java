0e5e42295c5fe59d29abbca8165072c00ee7551e
ssakura49 <79436433+ssakura49@users.noreply.github.com>
2025-04-18 19:30:09 +0800

    Update TConJEI.java

--- src/main/java/me/paypur/tconjei/TConJEI.java
+++ src/main/java/me/paypur/tconjei/TConJEI.java
@@ -37,6 +37,11 @@ public class TConJEI {
             StatlessMaterialStats.MAILLE.getIdentifier(),
             StatlessMaterialStats.SHIELD_CORE.getIdentifier()
     );
+    public static final List<MaterialStatsId> CURIO_STAT_IDS = List.of(
+            CharmChainMaterialStats.ID,
+            STExtraMaterialStats.CHARM_CORE.getType().getId()
+    );
+    
     public static HashMap<Item, Component> allMaterialsTooltip = new HashMap<>();
 
     public TConJEI() {
